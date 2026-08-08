846ff4b74113e0cc525c1d102b7ab742f3c8c001
Vansh Chaudhary <your-email@gmail.com>
2026-08-08 11:50:16 +0530

    Directory structure change

---
+++ Part-1/Part1-Basics.js
@@ -0,0 +1,73 @@
+// Data types in JS---
+// Number
+// Boolean
+// String
+// Undefined
+// null
+// Bigint
+// Symbol
+// a=25
+// typeof a// Output-'number'
+// name="tony stark"
+// typeof name // Output-'string'
+
+//=====================================
+//JS automatically detects the datatype
+//=====================================
+// NaN in JS-------------
+// The NaN global property is a value representing Not-A-Number
+// typeof NaN //output-'number'
+
+
+//==================================
+//----Operator Precedence-----------
+//==================================
+// () > ** > ,*/,% > +,-
+
+//===================
+//---------------------------
+// Let keyword---------
+// Syntax of declaring variables
+// let age=22;
+// age =age+1; now age=23
+
+//Const keyword-values of constants can't be changed with re-assignment and the can't be re-declared..
+// const year=2028
+// year=2027  //gives error
+// const pi=3.14
+
+//Var keyword- Old syntax of writing variables.
+// var age=23;
+// var cgpa=8.5;
+
+// ==============================
+//-----Identifier Rules----------
+// ==============================
+// All js variables must be identified with unique names(identifiers). 
+// *Names can contain letters,digits,underscores and dollar SVGStringList.apply(no space).
+// *Names must begins with a letter.  
+// *Names also begins with $ and_.
+// *Names are case sensitive.
+// *Reserved words(like Javascripts keywords) Cannot be used as names.
+
+//===================
+// String in JS------
+//===================
+// Strings are text or sequence of characters
+
+//========================
+//Null and Undefined in JS
+//========================
+// Undefined - A variable that has not been assigned a value is a type undefined
+// ex-
+// let a; O/P undefined
+//----------------------------
+// Null - The null value represents the intentional absence of any object value.
+// ex-
+// let a=null O/P-null
+
+
+
+
+
+  
