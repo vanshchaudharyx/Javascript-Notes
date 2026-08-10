82877223cb7785fd086e433e55be2d6c01048bbe
Vansh Chaudhary <your-email@gmail.com>
2026-08-10 15:06:09 +0530

    feat(js): add rest parameters and destructuring examples

--- Part-8/script.js
+++ Part-8/script.js
@@ -93,3 +93,59 @@ let aar = [0, 1, 3, 4, 6, 8, 9, 1, 3, 5, 7, 8];
 let minn = Math.min(...aar);
 console.log(minn); //O
 console.log(..."hello");
+
+//Rest=> Allows a function to take an indefinite number of arguements and bundle them in an array.
+function rest(...args) {
+  //arguements
+  for (let i = 0; i < args.length; i++) {
+    console.log("You give us:", args[i]);
+  }
+  // console.log(arguments);
+}
+function arg(a, b, c, d) {
+  console.log(arguments); // This argument is a collection.
+  console.log(arguments.length);
+  // arguments.push(5);// This shows error because push method is for array so we can say this is now array this is collection.
+}
+
+// function sum() {
+//   arguments.reduce((sum, el) /*This reduce method doesnt work with arguments because this is collection not array.*/
+//   => {
+//      return sum + el;
+//   });
+// }
+
+// First we take arguments with rest because it converts arguments in to array.
+function sum(...args) {
+  return args.reduce((sum, el) => sum + el);
+}
+
+//Destructuring=> Storing values of array into multiple variables.
+let names = ["tony", "bruce", "steve", "peter", "abc", "pyq", "xyz"];
+//instead of=>
+// let winner=names[0];
+// let runnerup=names[1];
+// let secondRunnerup=names[2];
+
+// We can do this with destructuring..
+let [winner, runnerup, secondRunnerup] = names;
+console.log(winner, runnerup, secondRunnerup);
+//lets suppose we have remaining values after assigning. for collecting that remaining that values we can use rest.
+let [win, run, ...others] = names;
+
+//Destructuring on objects-->
+const students = {
+  name: "Vansh",
+  class: "10th",
+  age: 20,
+  subjects: ["Math", "Science", "English"],
+  username: "vansh123",
+  password: "12345",
+};
+// let username=student.username;
+// let password=student.password;
+// This above way is not efficient if student have more properties.
+// by destructuring==>
+// let { username, password, ...otherss } = students;
+// if we want to change the variable name where we want to store the properties.
+let { username: user, password: pass, ...otherss } = students;
