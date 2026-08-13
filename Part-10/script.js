12255b1f02d874aa76ed2f359dc829d29e40f2d0
vanshchaudharyx <chaudharyvansh67@gmail.com>
2026-08-13 23:01:23 +0530

    feat(js): add event listeners and this in event handlers

--- Part-10/script.js
+++ Part-10/script.js
@@ -10,7 +10,37 @@ let btn2 = document.querySelector(".hello");
 function sayHello() {
   alert("Say Hello!");
 }
+function sayName() {
+  alert("Name!");
+}
 btn2.onclick = sayHello;
 btn2.onmouseenter = function () {
   console.log("You entered a sayhello button");
 };
+// If we want multiple event happen then that thing is not possible with onclick or onmouseenter.
+//Thats why we use Event listener
+
+//Event Listener-=>
+// addEventListener
+// element.addEventListener(event,callback)==>
+btn.addEventListener("click", function () {
+  console.log("button clicked");
+});
+let eventbtn = document.querySelector(".eventlist");
+eventbtn.addEventListener("click", sayHello);
+eventbtn.addEventListener("click", sayName);
+//These both will run line by line.
+
+//Event listener for elements.
+let para = document.querySelector("p");
+para.addEventListener("click", function () {
+  console.log("Para was clicked");
+});
+
+//this in Event Listeners=>
+// When 'this' is used in a callback of event handler of something,it refers to that something.
+//When we create event listener for any object then we use this at the place of callback.
+btn.addEventListener("click", function () {
+  console.log(this);//<button>Click me</button>
+  //This refers to the button.
+});
